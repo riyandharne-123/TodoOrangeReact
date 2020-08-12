518a41868d89ba9e636925364f4f6399e9e9c838
riyandharne-123 <60026968+riyandharne-123@users.noreply.github.com>
2020-08-12 13:52:29 +0530

    Updates

---
+++ precache-manifest.983ead9b9edda344860c026c704bddcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ffad393652c64cf763306192a29340",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87afe7ff1e1c51476343",
+    "url": "/static/css/main.b056db4f.chunk.css"
+  },
+  {
+    "revision": "239d05f7b6e2cc3dbea5",
+    "url": "/static/js/2.8ab7ccb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ab7ccb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87afe7ff1e1c51476343",
+    "url": "/static/js/main.b790061b.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
